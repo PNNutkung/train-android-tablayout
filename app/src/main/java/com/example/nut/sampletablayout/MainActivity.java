859a6cad708ef33a7cc83caacb95980de4d77009
PNNutkung <pipatpol.t@ku.th>
2016-06-10 14:35:48 +0700

    Initialize file checkin

---
+++ app/src/main/java/com/example/nut/sampletablayout/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.nut.sampletablayout;
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.main_tab_layout)
+    TabLayout mainTabLayout;
+    @BindView(R.id.main_view_pager)
+    ViewPager mainViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
+    }
+}
---
+++ app/src/main/java/com/example/nut/sampletablayout/ViewPagerAdapter.java
@@ -0,0 +1,7 @@
+package com.example.nut.sampletablayout;
+
+/**
+ * Created by nut on 10/6/2559.
+ */
+public class ViewPagerAdapter {
+}
